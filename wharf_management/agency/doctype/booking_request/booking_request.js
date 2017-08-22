f3c50505962d30a8c054d23f763dc0ab60e3ba89
staumoepeau <sione.taumoepeau@gmai.com>
2017-08-23 04:51:33 +1300

    Update 22 Aug 2017

--- wharf_management/agency/doctype/booking_request/booking_request.js
+++ wharf_management/agency/doctype/booking_request/booking_request.js
@@ -2,107 +2,110 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Booking Request', {
-	
-	setup: function(frm) {
-		frm.get_field('ship_requirements_link').grid.editable_fields = [
-			{fieldname: 'ship_requirements', columns: 2},
-			{fieldname: 'ship_requirements_yes_no', columns: 2},
-			{fieldname: 'ship_requirements_specify', columns: 2}
-		];
 
-		frm.get_field('cargo_booking_manifest_table').grid.editable_fields = [
-			{fieldname: 'cargo_type', columns: 1},
-			{fieldname: 'cargo_content', columns: 1},
-			{fieldname: 'dis_charging', columns: 1},
-			{fieldname: 'loading', columns: 1},
-			{fieldname: 'total_weight', columns: 1}
-		];
+    setup: function(frm) {
+        frm.get_field('ship_requirements_link').grid.editable_fields = [
+            { fieldname: 'ship_requirements', columns: 2 },
+            { fieldname: 'ship_requirements_yes_no', columns: 2 },
+            { fieldname: 'ship_requirements_specify', columns: 2 }
+        ];
 
-		frm.get_field('forklift_table').grid.editable_fields = [
-			{fieldname: 'forklift_require', columns: 1},
-			{fieldname: 'forklift_qty', columns: 1}
-		];
+        frm.get_field('cargo_booking_manifest_table').grid.editable_fields = [
+            { fieldname: 'cargo_type', columns: 1 },
+            { fieldname: 'cargo_content', columns: 1 },
+            { fieldname: 'dis_charging', columns: 1 },
+            { fieldname: 'loading', columns: 1 },
+            { fieldname: 'total_weight', columns: 1 }
+        ];
 
-	},
-	
-	
-	refresh: function(frm) {
-		
-		
-		if(!frm.doc.__islocal){
-			cur_frm.set_df_property("agents", "read_only", 1);
-			cur_frm.set_df_property("voyage_no", "read_only", 1);
-			cur_frm.set_df_property("vessel", "read_only", 1);
-			cur_frm.set_df_property("vessel_type", "read_only", 1);
+        frm.get_field('forklift_table').grid.editable_fields = [
+            { fieldname: 'forklift_require', columns: 1 },
+            { fieldname: 'forklift_qty', columns: 1 }
+        ];
 
-		}
-			
-		if(frappe.user.has_role("Wharf Operation User") || frappe.user.has_role("Wharf Operation Manager")){
-			frm.add_custom_button(__('Create Payment'), function() {
-				create_payment(frm);
-			});
-			
-		}
-		var Current_User = user
-		if(Current_User == frm.doc.owner){
-			frm.add_custom_button(__('Amend ETA'), function() {
-				create_payment(frm);
-			});
-			
-		}
-		
-	},
-	onload:function(frm){
-		var Current_User = user;
-//		if(frm.doc.__islocal){
-			if (Current_User != cur_frm.doc.owner & Current_User != "Administrator"){
-//				cur_frm.appframe.buttons.Print.remove();
-				
-				msgprint({title:__('Error'), indicator:'red',
-				message:__("You are not Authorise to view this transaction.")});
-				window.close();
-				
-			}
-//		}
-//		console.log(frm.doc.owner)
-		
-//		msgprint("Hello : %s", frm.doc.owner)
+    },
 
-	},
-	
-	
-//	total_weight_amount: function(frm) {
-//		var totalamount = flt(frm.doc.total_weight_amount * 0.5);
-//		frm.set_value("");	
-//	}
-	
-	
-});
 
-frappe.ui.form.on("Cargo Booking Manifest Table", "weight", function(frm, cdt, cdn){
-  var d = locals[cdt][cdn];
-  frappe.model.set_value(d.doctype, d.name, "total_weight", flt(d.weight));
+    refresh: function(frm) {
 
-  var total_weight_amount = 0;
-  var totalamount = 0;
-  frm.doc.cargo_booking_manifest_table.forEach(function(d) { flt(total_weight_amount += flt(d.weight)); });
-	
-  frm.set_value("total_weight_amount", total_weight_amount);
+
+        if (!frm.doc.__islocal) {
+            cur_frm.set_df_property("agents", "read_only", 1);
+            cur_frm.set_df_property("voyage_no", "read_only", 1);
+            cur_frm.set_df_property("vessel", "read_only", 1);
+            cur_frm.set_df_property("vessel_type", "read_only", 1);
+
+        }
+
+        if (frappe.user.has_role("Wharf Operation User") || frappe.user.has_role("Wharf Operation Manager")) {
+            frm.add_custom_button(__('Create Payment'), function() {
+                create_payment(frm);
+            });
+
+        }
+        var Current_User = user
+        if (Current_User == frm.doc.owner) {
+            frm.add_custom_button(__('Amend ETA'), function() {
+                create_payment(frm);
+            });
+
+        }
+
+    },
+    onload: function(frm) {
+        var Current_User = user;
+        //		if(frm.doc.__islocal){
+        if (Current_User != cur_frm.doc.owner & Current_User != "Administrator") {
+            //				cur_frm.appframe.buttons.Print.remove();
+
+            msgprint({
+                title: __('Error'),
+                indicator: 'red',
+                message: __("You are not Authorise to view this transaction.")
+            });
+            window.close();
+            window.closed;
+        }
+        //		}
+        //		console.log(frm.doc.owner)
+
+        //		msgprint("Hello : %s", frm.doc.owner)
+
+    },
+
+
+    //	total_weight_amount: function(frm) {
+    //		var totalamount = flt(frm.doc.total_weight_amount * 0.5);
+    //		frm.set_value("");	
+    //	}
+
 
 });
 
-var create_payment = function(frm){
-		frappe.call({
-			"method": "frappe.client.get",
-				args: {
-					doctype: "Payment Entry",
-					filters: {'payment_ref': frm.doc.booking_ref},
-				name: frm.doc.booking_ref
-					},
-				callback: function (data) {
-						frappe.route_options = { "payment_ref": frm.doc.name }
-						frappe.new_doc("Payment Entry");
-						frappe.set_route("Form", "Payment Entry", doc.name);
-				}
-		})				
-	}
+frappe.ui.form.on("Cargo Booking Manifest Table", "weight", function(frm, cdt, cdn) {
+    var d = locals[cdt][cdn];
+    frappe.model.set_value(d.doctype, d.name, "total_weight", flt(d.weight));
+
+    var total_weight_amount = 0;
+    var totalamount = 0;
+    frm.doc.cargo_booking_manifest_table.forEach(function(d) { flt(total_weight_amount += flt(d.weight)); });
+
+    frm.set_value("total_weight_amount", total_weight_amount);
+
+});
+
+var create_payment = function(frm) {
+    frappe.call({
+        "method": "frappe.client.get",
+        args: {
+            doctype: "Payment Entry",
+            filters: { 'payment_ref': frm.doc.booking_ref },
+            name: frm.doc.booking_ref
+        },
+        callback: function(data) {
+            frappe.route_options = { "payment_ref": frm.doc.name }
+            frappe.new_doc("Payment Entry");
+            frappe.set_route("Form", "Payment Entry", doc.name);
+        }
+    })
+}
