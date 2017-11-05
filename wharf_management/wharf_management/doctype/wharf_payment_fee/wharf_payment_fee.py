5b408c464ba5e6273098440fba356f1f00cefb0f
root <root@localhost.localdomain>
2017-11-06 09:47:32 +1400

    Update

--- wharf_management/wharf_management/doctype/wharf_payment_fee/wharf_payment_fee.py
+++ wharf_management/wharf_management/doctype/wharf_payment_fee/wharf_payment_fee.py
@@ -21,6 +21,7 @@ class WharfPaymentFee(Document):
 		self.update_payment_status()
 #		self.update_export_status()
 		self.change_status()
+#		self.make_entries()
 		
 	
 #	def update_bulk_payment(self):
@@ -164,3 +165,36 @@ class WharfPaymentFee(Document):
     			fees=0
 
 		self.total_fee = float((self.storage_days_charged * vals.standard_rate)+(1 * val.standard_rate)+(1 * fees))
+				
+
+
+#    def make_entries(self, cancel=0, adv_adj=0):
+#		frappe.db.sql("""Update `tabBooking Request` set payment_status="Paid" , workflow_state="Booking Paid" where name=%s""", (self.booking_ref))
+#		from erpnext.accounts.general_ledger import make_gl_entries
+#		gl_map = []
+			
+#		gl_map.append(
+#			frappe._dict({
+#				"posting_date": self.posting_date,
+#				"account": self.paid_to,
+#				"account_currency": self.company_currency,
+#				"debit": self.paid_amount,
+#				"voucher_type": self.doctype,
+#				"voucher_no": self.name,
+#				"against": self.party_name
+#			}))
+#		gl_map.append(
+#			frappe._dict({
+#				"posting_date": self.posting_date,
+#				"account": self.paid_from,
+#				"credit": self.paid_amount,
+#				"voucher_type": self.doctype,
+#				"voucher_no": self.name,
+#				"against": self.paid_to,
+#				"party_type": self.party_type,
+#				"party": self.consignee
+#			}))
+
+#		if gl_map:
+#			make_gl_entries(gl_map, cancel=(self.docstatus == 2))	
+	
