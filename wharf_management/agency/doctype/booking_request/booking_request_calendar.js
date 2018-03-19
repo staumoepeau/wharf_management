72dc86d700e12a7f34981bd3940f60157c81f7c9
staumoepeau <sione.taumoepeau>
2018-03-19 14:47:02 +1300

    Calendar View

---
+++ wharf_management/agency/doctype/booking_request/booking_request_calendar.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2018, Sione Taumoepeau and contributors
+// For license information, please see license.txt
+
+
+frappe.views.calendar["Booking Request"] = {
+	field_map: {
+		"start": "eta_date",
+		"end": "etd_date",
+		"id": "name",
+		"title": "vessel",
+		"allDay": "allDay",
+		"status": "status"
+	},
+	gantt: true,
+//	filters: [
+//		{
+//			"fieldtype": "Link",
+//			"fieldname": "project",
+//			"options": "Project",
+//			"label": __("Project")
+//		}
+//	],
+	get_events_method: "frappe.desk.calendar.get_events"
+}
