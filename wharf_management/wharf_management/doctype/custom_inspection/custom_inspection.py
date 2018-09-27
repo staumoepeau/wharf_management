55db6176a1476fef2c1cac88d1dbd7833930a54e
staumoepeau <sione.taumoepeau>
2018-09-28 09:53:21 +1300

    Fix Custom Inspection Error

--- wharf_management/wharf_management/doctype/custom_inspection/custom_inspection.py
+++ wharf_management/wharf_management/doctype/custom_inspection/custom_inspection.py
@@ -14,20 +14,22 @@ class CustomInspection(Document):
 
 			
 	def update_custom_inspection_status(self):
-		if not self.status or self.status != "Deliver":
-			self.status = self.get_status()
+#		if not self.status or self.status != "Deliver":
+#			self.status = self.get_status()
 		
-		if not self.movement or self.movement != "Completed":
-			self.inspection_status = self.get_inspection_status()
+#		if not self.movement or self.movement != "Completed":
+#			self.inspection_status = self.get_inspection_status()
 		
-			frappe.db.sql("""Update `tabCargo` set yard_slot=NULL, custom_inspection_status='Closed' where name=%s""", (self.cargo_ref))
+		frappe.db.sql("""Update `tabCargo` set yard_slot=NULL, custom_inspection_status='Closed' where name=%s""", (self.cargo_ref))
+		frappe.db.sql("""Update `tabCustom Inspection` set status="Deliver", movement='Completed0' where name=%s""", (self.name))
+
 
 	
-	def get_status(self):
-		mystatus = "Deliver"
-		return mystatus
+#	def get_status(self):
+#		mystatus = "Deliver"
+#		return mystatus
 	
-	def get_inspection_status(self):
-		inspection_status = "Completed"
-		return inspection_status
+#	def get_inspection_status(self):
+#		inspection_status = "Completed"
+#		return inspection_status
 		
