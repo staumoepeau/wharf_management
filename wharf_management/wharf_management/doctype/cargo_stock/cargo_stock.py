d1ece1d9dd33318bca58db676b2a63aa8444fb3c
staumoepeau <sione.taumoepeau>
2018-10-10 10:46:54 +1300

    Fix Stock error

--- wharf_management/wharf_management/doctype/cargo_stock/cargo_stock.py
+++ wharf_management/wharf_management/doctype/cargo_stock/cargo_stock.py
@@ -36,13 +36,13 @@ class CargoStock(Document):
 	def check_export(self):
 		cargo_exist=None
 
-		if self.cargo_type == "Container":
+		if self.cargo_type in ["Container", "Tank Tainer", "Flatrack"]:
 			cargo_exist = frappe.db.sql("""Select name from `tabExport` where container_no=%s""", (self.container_no))
 
 		if self.cargo_type in ["Loose Cargo", "Break Bulk"]:
 			cargo_exist  = frappe.db.sql("""Select name from `tabExport` where mark=%s""", (self.mark))
 		
-		if self.cargo_type == "Vehicles":
+		if self.cargo_type in ["Vehicles", "Heavy Vehicles"]:
 			cargo_exist  = frappe.db.sql("""Select name from `tabExport` where chasis_no=%s""", (self.chasis_no))
 
 		if cargo_exist:
@@ -52,13 +52,13 @@ class CargoStock(Document):
 	def check_cargo_loading(self):
 		cargo_exist=None
 
-		if self.cargo_type == "Container":
+		if self.cargo_type in ["Container", "Tank Tainer", "Flatrack"]:
 			cargo_exist = frappe.db.sql("""Select name from `tabCargo` where ((%s - eta_date) < 30) and status="Outbound" and manifest_check != "Confirm" and work_type="Loading" and container_no=%s""", (self.yard_date, self.container_no))
 			
 		if self.cargo_type in ["Loose Cargo", "Break Bulk"]:
 			cargo_exist  = frappe.db.sql("""Select name from `tabCargo` where ((%s - eta_date) < 30) and status="Outbound" and manifest_check != "Confirm" and work_type="Loading" and mark=%s""", (self.yard_date, self.mark))
 		
-		if self.cargo_type == "Vehicles":
+		if self.cargo_type in ["Vehicles", "Heavy Vehicles"]:
 			cargo_exist = frappe.db.sql("""Select name from `tabCargo` where ((%s - eta_date) < 30) and status="Outbound" and manifest_check != "Confirm" and work_type="Loading" and chasis_no=%s""", (self.yard_date, self.chasis_no))
 
 		if cargo_exist:
@@ -67,13 +67,13 @@ class CargoStock(Document):
 
 	def check_cargo_discharged(self):
 		cargo_exist=None
-		if self.cargo_type == "Container":
+		if self.cargo_type in ["Container", "Tank Tainer", "Flatrack"]:
 			cargo_exist = frappe.db.sql("""Select name from `tabCargo` where ((%s - eta_date) < 30) and manifest_check != "Confirm" and work_type="Discharged" and container_no=%s""", (self.yard_date, self.container_no))
 			
 		if self.cargo_type in ["Loose Cargo", "Break Bulk"]:
 			cargo_exist = frappe.db.sql("""Select name from `tabCargo` where ((%s - eta_date) < 30) and manifest_check != "Confirm" and work_type="Discharged" and and mark=%s""", (self.yard_date, self.mark))
 		
-		if self.cargo_type == "Vehicles":
+		if self.cargo_type in ["Vehicles", "Heavy Vehicles"]:
 			cargo_exist = frappe.db.sql("""Select name from `tabCargo` where ((%s - eta_date) < 30) and manifest_check != "Confirm" and work_type="Discharged" and chasis_no=%s""", (self.yard_date, self.chasis_no))
 
 		if not cargo_exist:
