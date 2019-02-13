9f357002521db2fd05fa5c847a0e5015dcf295eb
staumoepeau <sione.taumoepeau>
2019-02-13 17:07:52 +1300

    Update to Frappe version 11

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+#from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in wharf_management/__init__.py
@@ -10,7 +10,7 @@ with open('wharf_management/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='wharf_management',
@@ -21,6 +21,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
+#	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
