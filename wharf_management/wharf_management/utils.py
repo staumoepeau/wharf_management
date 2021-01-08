33dbfa60017fee544d8eb0c00a6ace43a6586f95
staumoepeau <sione.taumoepeau@gmail.com>
2021-01-09 11:19:46 +1300

    Fix Error

--- wharf_management/wharf_management/utils.py
+++ wharf_management/wharf_management/utils.py
@@ -115,7 +115,7 @@ def get_create_cargo_devan(doctype, cargo_ref, final_work_type, secondary_work_t
 def get_create_cargo(doctype, cargo_ref, final_work_type, secondary_work_type, cargo_type):
 
     last_work_type, third_work_type, movement, payment, gate, yard_status = "", "", "", "", "", ""
-    container_content, inspection_status, yard_date, final_status = "", "", "", ""
+    container_content, inspection_status, yard_date, final_status, secondary_work_type = "", "", "", "", ""
 
     
     if doctype == "Pre Advice":
