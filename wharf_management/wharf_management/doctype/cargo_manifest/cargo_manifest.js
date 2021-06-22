b23f9d1aa3c78495fdf1e287a46ec02846baf266
root <root@tongaports.to>
2021-06-23 11:23:15 +1300

    Fix Cargo Manifest

--- wharf_management/wharf_management/doctype/cargo_manifest/cargo_manifest.js
+++ wharf_management/wharf_management/doctype/cargo_manifest/cargo_manifest.js
@@ -3,104 +3,165 @@
 
 frappe.ui.form.on('Cargo Manifest', {
 
-	setup: function(frm) {
-		frm.get_field('cargo_manifest_table').grid.editable_fields = [
-			{fieldname: 'cargo_refrence', columns: 1},
-			{fieldname: 'container_no', columns: 1},
-			{fieldname: 'cargo_type', columns: 1},
-			{fieldname: 'work_type', columns: 1},
-			{fieldname: 'container_content', columns: 1},
-			{fieldname: 'manifest_confirm', columns: 1}
-		];
+    setup: function(frm) {
+        frm.get_field('cargo_manifest_table').grid.editable_fields = [
+            { fieldname: 'cargo_refrence', columns: 1 },
+            { fieldname: 'container_no', columns: 1 },
+            { fieldname: 'cargo_type', columns: 1 },
+            { fieldname: 'work_type', columns: 1 },
+            { fieldname: 'container_content', columns: 1 },
+            { fieldname: 'manifest_confirm', columns: 1 }
+        ];
 
-		frm.get_field('manifest_summary_table').grid.editable_fields = [
-			{fieldname: 'cargo_type', columns: 1},
-			{fieldname: 'container_content', columns: 1},
-			{fieldname: 'work_type', columns: 1},
-			{fieldname: 'container_size', columns: 1},
-			{fieldname: 'number', columns: 1},
-			{fieldname: 'handling_fee', columns: 1},
-			{fieldname: 'handling_fee_discount', columns: 1}
-		];
+        frm.get_field('manifest_summary_table').grid.editable_fields = [
+            { fieldname: 'cargo_type', columns: 1 },
+            { fieldname: 'container_content', columns: 1 },
+            { fieldname: 'work_type', columns: 1 },
+            { fieldname: 'container_size', columns: 1 },
+            { fieldname: 'number', columns: 1 },
+            { fieldname: 'handling_fee', columns: 1 },
+            { fieldname: 'handling_fee_discount', columns: 1 }
+        ];
 
-		frm.get_field('bbulks_summary_table').grid.editable_fields = [
-			{fieldname: 'cargo_type', columns: 1},
-			{fieldname: 'work_type', columns: 1},
-			{fieldname: 'weight', columns: 1},
-			{fieldname: 'volume', columns: 1},
-			{fieldname: 'number', columns: 1},
-			{fieldname: 'handling_fee', columns: 1}
-		];
-	},
+        frm.get_field('bbulks_summary_table').grid.editable_fields = [
+            { fieldname: 'cargo_type', columns: 1 },
+            { fieldname: 'work_type', columns: 1 },
+            { fieldname: 'weight', columns: 1 },
+            { fieldname: 'volume', columns: 1 },
+            { fieldname: 'number', columns: 1 },
+            { fieldname: 'handling_fee', columns: 1 }
+        ];
+    },
 
-	refresh: function(frm) {
+    refresh: function(frm) {
 
 
-	},
+    },
 
-	booking_ref: function(frm){
-		frappe.call({
-			method:"frappe.client.get",
-				args: {
-						doctype:"Booking Request",
-						filters: {'name': frm.doc.booking_ref
-						},
-					},
-				callback: function(r) {
-					cur_frm.set_value("voyage_no", r.message["voyage_no"])
-					cur_frm.set_value("agents", r.message["agents"]);
-					cur_frm.set_value("vessel", r.message["vessel"]);
-					cur_frm.set_value("eta_date", r.message["eta_date"]);
-					cur_frm.set_value("etd_date", r.message["etd_date"]);
-					cur_frm.set_value("pod", r.message["pod"]);
-					cur_frm.set_value("pol", r.message["pol"]);
-					cur_frm.set_value("final_dest_port", r.message["final_dest_port"]);
+    booking_ref: function(frm) {
+        frappe.call({
+            method: "frappe.client.get",
+            args: {
+                doctype: "Booking Request",
+                filters: {
+                    'name': frm.doc.booking_ref
+                },
+            },
+            callback: function(r) {
+                cur_frm.set_value("voyage_no", r.message["voyage_no"])
+                cur_frm.set_value("agents", r.message["agents"]);
+                cur_frm.set_value("vessel", r.message["vessel"]);
+                cur_frm.set_value("eta_date", r.message["eta_date"]);
+                cur_frm.set_value("etd_date", r.message["etd_date"]);
+                cur_frm.set_value("pod", r.message["pod"]);
+                cur_frm.set_value("pol", r.message["pol"]);
+                cur_frm.set_value("final_dest_port", r.message["final_dest_port"]);
 
-				}
-		})
-		
-	},
-	apply_manifest_fee: function(frm){
-		if (frm.doc.apply_manifest_fee == 1){
-			cur_frm.set_value("manifest_fee", "600.00");
-		}
-		else {
-			cur_frm.set_value("manifest_fee", );
-		}
-	},
-	
-	
-	get_manifest_list: function(frm) {
-		return frappe.call({
-			method: "get_manifest",
-			doc: frm.doc,
-			callback: function(t) {
-				frm.refresh_field("cargo_manifest_table");
-				frm.refresh_fields();
-			}
-		});
-	},
+            }
+        })
 
-	get_summary_list: function(frm) {
-		return frappe.call({
-			method: "get_manifest_summary_list",
-			doc: frm.doc,
-			callback: function(r) {
-				frm.refresh_field("manifest_summary_table");
-				frm.refresh_fields();
-			}
-		});
-	},
+    },
+    apply_manifest_fee: function(frm) {
+        if (frm.doc.apply_manifest_fee == 1) {
+            cur_frm.set_value("manifest_fee", "600.00");
+        } else {
+            cur_frm.set_value("manifest_fee", );
+        }
+    },
 
-	get_bbulks_summary_list: function(frm) {
-		return frappe.call({
-			method: "get_bbulks_summary_list",
-			doc: frm.doc,
-			callback: function(r) {
-				frm.refresh_field("bbulks_summary_table");
-				frm.refresh_fields();
-			}
-		});
-	},
+
+    get_manifest_list: function(frm) {
+        display_manifest_list(frm);
+    },
+
+    get_summary_list: function(frm) {
+        display_summary_list(frm);
+    },
+
+    get_bbulks_summary_list: function(frm) {
+        display_bbulk_summary_list(frm);
+    },
 });
-	
+
+var display_manifest_list = function(frm) {
+    frappe.call({
+        method: "wharf_management.wharf_management.doctype.cargo_manifest.cargo_manifest.get_manifest",
+        args: {
+            "booking_ref": frm.doc.booking_ref
+        },
+        callback: function(r) {
+            if (r.message) {
+                $.each(r.message, function(i, item) {
+                    var item_row = frm.add_child("cargo_manifest_table")
+                    item_row.container_size = item.container_size,
+                        item_row.container_no = item.container_no,
+                        item_row.cargo_type = item.cargo_type,
+                        item_row.work_type = item.work_type,
+                        item_row.container_content = item.container_content,
+                        item_row.status = item.status,
+                        item_row.container_size = item.container_size,
+                        item_row.voyage_no = item.voyage_no,
+                        item_row.handling_fee = item.handling_fee,
+                        item_row.storage_fee = item.storage_fee,
+                        item_row.chasis_no = item.chasis_no,
+                        item_row.mark = item.mark,
+                        item_row.manifest_check = item.manifest_check,
+                        item_row.booking_ref = item.booking_ref,
+                        item_row.cargo_refrence = item.cargo_refrence
+                });
+                frm.refresh()
+            }
+        }
+    });
+}
+
+var display_summary_list = function(frm) {
+    frappe.call({
+        method: "wharf_management.wharf_management.doctype.cargo_manifest.cargo_manifest.get_manifest_summary_list",
+        args: {
+            "booking_ref": frm.doc.booking_ref
+        },
+        callback: function(r) {
+            if (r.message) {
+                $.each(r.message, function(i, item) {
+                    var item_row = frm.add_child("manifest_summary_table")
+                    item_row.cargo_type = item.cargo_type,
+                        item_row.container_content = item.container_content,
+                        item_row.work_type = item.work_type,
+                        item_row.container_size = item.container_size,
+                        item_row.handling_fee_discount = item.handling_fee_discount,
+                        item_row.number = item.number,
+                        item_row.handling_fee = item.handling_fee,
+                        item_row.storage_fee = item.storage_fee,
+                        item_row.wharfage_fee = item.wharfage_fee
+                });
+                frm.refresh()
+            }
+        }
+    });
+}
+
+var display_bbulk_summary_list = function(frm) {
+    frappe.call({
+        method: "wharf_management.wharf_management.doctype.cargo_manifest.cargo_manifest.get_bbulks_summary_list",
+        args: {
+            "booking_ref": frm.doc.booking_ref
+        },
+        callback: function(r) {
+            if (r.message) {
+                $.each(r.message, function(i, item) {
+                    var item_row = frm.add_child("bbulks_summary_table")
+                    item_row.cargo_type = item.cargo_type,
+                        item_row.work_type = item.work_type,
+                        item_row.number = item.number,
+                        item_row.weight = item.weight,
+                        item_row.volume = item.volume,
+                        item_row.handling_fee = item.handling_fee,
+                        item_row.storage_fee = item.storage_fee,
+                        item_row.wharfage_fee = item.wharfage_fee
+                });
+                frm.refresh()
+            }
+        }
+    });
+}
--- wharf_management/wharf_management/doctype/cargo_manifest/cargo_manifest.py
+++ wharf_management/wharf_management/doctype/cargo_manifest/cargo_manifest.py
@@ -11,8 +11,9 @@ from frappe.model.document import Document
 
 class CargoManifest(Document):
 
-	#	def validate(self):
-	#		self.cretate_manifest_list()
+	def validate(self):
+		if not self.booking_ref:
+			frappe.msgprint(_("Please Check Booking Ref"))
 
 #		def on_submit(self):
 #			self.update_cargo_table()
@@ -22,80 +23,32 @@ class CargoManifest(Document):
 #			frappe.db.sql("""Update `tabCargo` set vessel = %s, voyage_no = %s where parent = %s""", (self.vessel, self.voyage_no, self.name))
 #			frappe.db.sql("""Update `tabCargo` set status='Unknown' where parent = %s""", (self.name))
 
+@frappe.whitelist()
+def get_manifest(booking_ref):	
+	return frappe.db.sql("""SELECT name as cargo_refrence, container_no, bol, cargo_type, chasis_no, mark,
+	work_type, container_size, container_content, status, voyage_no, booking_ref, storage_fee, handling_fee, manifest_check 
+	FROM `tabCargo` 
+	WHERE booking_ref = %s 
+	AND docstatus = 1
+	AND manifest_check = 'Confirm'""",(booking_ref), as_dict=1)
 
-		def get_manifest(self):
-		
-			manifest_entries = frappe.db.sql("""SELECT name as cargo_refrence, container_no, bol, cargo_type, chasis_no, mark,
-			work_type, container_size, container_content, status, voyage_no, booking_ref, storage_fee, handling_fee, manifest_check 
-			FROM `tabCargo` 
-			WHERE booking_ref = %s 
-			AND docstatus = 1
-			AND manifest_check = 'Confirm'""",(self.booking_ref), as_dict=1)
-
-#			manifest_entries = sorted(list(cargo_manifest_table))
-
-			self.set('cargo_manifest_table', [])
-
-			for f in manifest_entries:
-				row = self.append('cargo_manifest_table', {
-					'container_size':f.container_size,
-					'container_no':f.container_no,
-					'cargo_type':f.cargo_type,
-					'work_type':f.work_type,
-					'container_content':f.container_content,
-					'status':f.status,
-					'container_size':f.container_size,
-					'voyage_no':f.voyage_no,
-					'handling_fee' : f.handling_fee,
-					'storage_fee' : f.storage_fee,
-					'chasis_no': f.chasis_no,
-					'mark': f.mark,
-					'manifest_check': f.manifest_check,
-					'booking_ref':f.booking_ref,
-					'cargo_refrence': f.cargo_refrence
-				})
-
-
-		def get_manifest_summary_list(self):
+@frappe.whitelist()
+def get_manifest_summary_list(booking_ref):
 			
-#			manifest_summary_table = frappe.db.sql("""select cargo_type, container_content, work_type, container_size, count(name) as container
-#				from `tabCargo` where cargo_type = "Container" and booking_ref = %s and final_status in ("Discharged","Loading") group by work_type, container_content, container_size""", (self.booking_ref), as_dict=1)
-			entries = frappe.db.sql("""select cargo_type, work_type, container_size, container_content, handling_fee_discount, sum(handling_fee) as handling_fee, sum(storage_fee) as storage_fee, sum(wharfage_fee) as wharfage_fee, count(name) as number from `tabCargo` 
-			 where booking_ref = %s and manifest_check="Confirm" and cargo_type in ("Container","Split Ports","Petrolium","Tank Tainers") group by work_type, cargo_type, container_content, container_size, handling_fee_discount""", (self.booking_ref), as_dict=1)
-#			entries = sorted(list(manifest_summary_table))
+	return frappe.db.sql("""SELECT cargo_type, work_type, container_size, container_content, handling_fee_discount, 
+	sum(handling_fee) as handling_fee, sum(storage_fee) as storage_fee, sum(wharfage_fee) as wharfage_fee, count(name) as number 
+	FROM `tabCargo` 
+	WHERE booking_ref = %s 
+	AND manifest_check="Confirm" 
+	AND cargo_type in ("Container","Split Ports","Petrolium","Tank Tainers") 
+	GROUP BY work_type, cargo_type, container_content, container_size, handling_fee_discount""", (booking_ref), as_dict=1)
 
-			self.set('manifest_summary_table', [])
-
-			for d in entries:
-				row = self.append('manifest_summary_table', {
-					'cargo_type': d.cargo_type,
-					'container_content': d.container_content,
-					'work_type': d.work_type,
-					'container_size': d.container_size,
-					'handling_fee_discount': d.handling_fee_discount,
-					'number' : d.number,
-					'handling_fee':d.handling_fee,
-					'storage_fee' : d.storage_fee,
-					'wharfage_fee' : d.wharfage_fee
-				})
-			
-		def get_bbulks_summary_list(self):
-			
-			bbulks_entries = frappe.db.sql("""select cargo_type, work_type, count(name) as number, sum(net_weight) as weight, sum(volume) as volume, sum(handling_fee) as handling_fee, sum(storage_fee) as storage_fee, sum(wharfage_fee) as wharfage_fee 
-			from `tabCargo` where cargo_type in ("Break Bulk","Loose Cargo","Vehicles","Heavy Vehicles") and booking_ref = %s and manifest_check="Confirm" group by cargo_type, work_type and docstatus = 1""", (self.booking_ref), as_dict=1)
-
-#			bbulks_entries = sorted(list(bbulks_summary_table))
-
-			self.set('bbulks_summary_table', [])
-
-			for b in bbulks_entries:
-				row = self.append('bbulks_summary_table', {
-					'cargo_type': b.cargo_type,
-					'work_type': b.work_type,
-					'number' : b.number,
-					'weight':b.weight,
-					'volume':b.volume,
-					'handling_fee':b.handling_fee,
-					'storage_fee' : b.storage_fee,
-					'wharfage_fee' : b.wharfage_fee
-				})
+@frappe.whitelist()
+def get_bbulks_summary_list(booking_ref):
+	return frappe.db.sql("""SELECT cargo_type, work_type, count(name) as number, 
+	sum(net_weight) as weight, sum(volume) as volume, sum(handling_fee) as handling_fee, 
+	sum(storage_fee) as storage_fee, sum(wharfage_fee) as wharfage_fee 
+	FROM `tabCargo` 
+	WHERE cargo_type in ("Break Bulk","Loose Cargo","Vehicles","Heavy Vehicles") 
+	AND booking_ref = %s 
+	AND manifest_check="Confirm" group by cargo_type, work_type and docstatus = 1""", (booking_ref), as_dict=1)
